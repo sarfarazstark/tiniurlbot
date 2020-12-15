f2e95ab9a429c091d056cf4ce2ef30ec10130bc9
Rithunand <73537720+Benchamxd@users.noreply.github.com>
2020-12-15 14:48:31 +0530

    Update index.php

--- Bot/index.php
+++ Bot/index.php
@@ -55,9 +55,13 @@ $short = $get['shortenedUrl'];
 if($get['shortenedUrl']){
 bot('sendmessage', [
                 'chat_id' =>$chat_id,
-                'text'=>"Thank you for using me☺
+                'text'=>"**Thank you for using me☺
 
-HERE IS YOUR SHORTEN <a href="$short">LINK</a>",
+HERE IS YOUR SHORTEN LINK:-**`$short`
+
+**LONG URL:** `$text`
+
+**Shorten by $BOT_USERNAME**",
    'parse_mode'=>"HTML",
 ]);
    
