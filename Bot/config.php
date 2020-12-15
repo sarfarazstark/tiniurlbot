d9127be8d0db34d8a422f770f6d4189b1669cb0f
Rithunand <73537720+Benchamxd@users.noreply.github.com>
2020-12-15 15:17:40 +0530

    Update config.php

--- Bot/config.php
+++ Bot/config.php
@@ -7,4 +7,11 @@ $BOT_USERNAME = $_ENV['BOT_USERNAME'];
 
 //=========GPLINKS API KEY=========//
 $GP_API_KEY = $_ENV['GP_API_KEY'];
+
+//=========OTHERS=======//
+$START_MESSAGE = $_ENV["START_MESSAGE"] = <<<EOM
+Thank you for using me
+
+<a href="https://github.com/BENCHAMXD/GPLINKGPLINKSBOT">TEST</a>
+EOM;
 ?>
