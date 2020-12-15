c4340d3493d6329be07fa55307da1aeb23359276
Rithunand <73537720+Benchamxd@users.noreply.github.com>
2020-12-15 12:53:20 +0530

    Update config.php

--- Bot/config.php
+++ Bot/config.php
@@ -1,3 +1,11 @@
 <?php
+//=========BOT TOKEN==========//
+$BOT_TOKEN = $_ENV['BOT_TOKEN];
+
+//=========BOT USERNAME======//
+$BOT_USERNAME = $_ENV['BOT_USERNAME'];
+
+//=========GPLINKS API KEY=========//
+$GP_API_KEY = $_ENV['GP_API_KEY'];
 
 ?>
